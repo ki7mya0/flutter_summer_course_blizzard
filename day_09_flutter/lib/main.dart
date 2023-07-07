a541e1b08570197affbf8e0511963829023fef8e
Blizzard-12 <blizzard-12@Admin-12s-iMac.local>
2023-07-07 13:01:30 +0800

    add: day09

---
+++ day_09_flutter/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  print('Hello Flutter');
+  runApp(
+      const MaterialApp(
+      home: Scaffold(
+    body: Center(child: Text('data')),
+  )));
+}
