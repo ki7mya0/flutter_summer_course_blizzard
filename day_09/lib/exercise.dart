0bb7c8d3badc0cb4612c5affa9c44bf58d23971d
Blizzard-12 <blizzard-12@Admin-12s-iMac.local>
2023-07-07 13:02:35 +0800

    add: day09

--- day_09/lib/exercise.dart
+++ day_09/lib/exercise.dart
@@ -1,12 +1,19 @@
 import 'dart:io';
 
 class Cat {
-
+ final String _asciiArt = '''
+  /\\_/\\
+  ( o.o )
+   > ^ <
+   ''';
   int x = 0;
   int y = 0;
+  void display(){
+    _asciiArt =
+  }
 
 }
 
 void main(){
-  final
+
 }
