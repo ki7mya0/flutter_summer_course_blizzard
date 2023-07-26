7d68d4d9d44df6d36a4a56a2a1ca6d9fd2afba4a
Blizzard-12 <blizzard-12@Admin-12s-iMac.local>
2023-07-26 13:30:50 +0800

    day 14 startPage

--- wordfind_app/lib/main.dart
+++ wordfind_app/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:wordfind_app/start_page.dart';
 import 'package:wordfind_app/welcome_page.dart';
 
 void main() {
   MaterialApp wordFindGame = MaterialApp(
     title: 'Word Find Game',
     theme: ThemeData(fontFamily: 'Ribeye'),
-    home: const WelcomePage());
+    home: const StartPage());
   runApp(wordFindGame);
 }
---
+++ wordfind_app/lib/start_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:wordfind_app/Gradient_letter.dart';
+import 'package:wordfind_app/gradient_text.dart';
+
+class StartPage extends StatelessWidget {
+  const StartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+          ),
+    ));
+  }
+}
